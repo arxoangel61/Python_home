4b35014c3fb1402b02ffcdde42da54516bb1ee8e
arxoangel61 <56268062+arxoangel61@users.noreply.github.com>
2020-10-12 13:55:01 +0300

    Delete lesson1.1.py

--- lesson1.1.py
+++
@@ -1,8 +0,0 @@
-name = input("enter name: ")
-lastname = input("enter lastname: ")
-city = input("enter city: ")
-age = input("enter age: ")
-hobby = input("enter hobby:")
-print(f" My name {name.title()} \n My lastname {lastname.title()}\n "
-      f"I live in the city {city.title()}\n My age {age}\n My hobby {hobby.title()}")
-
